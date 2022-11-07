a5fdf9cf2f007c1a35af645b28a14ac863a79aae
onuraydin98 <32241352+onuraydin98@users.noreply.github.com>
2022-11-07 21:31:55 +0300

    cleared package.json

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose')
 const scraperList = require('./scraperList')
 const Book = require('./models/books')
 
+const enums = ['Idefix', 'BKMKitap', 'DR', 'KitapYurdu']
+
 const scrapingJob = async () => {
     await mongoose.connect(process.env.DATABASE_URL)
     console.log('Database connected')
+
+    enums.map((e) => {
+        console.log(e)
+    })
 
     // Practice -- will change
     let data = await scraperList['Idefix'].init()
